a46442fd54feb430f7e63e84afcc751957fbe70b
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-11-24 20:52:33 -0300

    Enforce code style and define routes in comments

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,15 @@ import helmet from '@fastify/helmet';
 const http = fastify();
 
 http.register(cors);
-http.register(helmet)
+http.register(helmet);
 
-http.get('/status', () => ({ ok: true }))
+http.get('/status', () => ({ ok: true }));
 
-http.listen({ port: 3333 }).then(() => console.log(':> server is running at http://localhost:3333/'))
+// FIRST http.post('/v1/user/create', (request, reply) => {});
+// http.post('/v1/user/auth', (request, reply) => {});
+
+// If user is authenticated, then they can see other users :)
+// FIRST http.get('/v1/users', (request, reply) => {});
+
+http.listen({ port: 3333 })
+  .then(() => console.log(':> server is running at http://localhost:3333/'));
