910e701e96769a9309fdc1450c8a9e44934a5ac6
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-11-24 21:08:40 -0300

    Implement /v1/user/create route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import helmet from '@fastify/helmet';
+import { z } from 'zod';
+import { db } from './database/index.js';
+import { user } from './database/schema.js';
+import { eq } from 'drizzle-orm';
 
 const http = fastify();
 
@@ -9,7 +13,25 @@ http.register(helmet);
 
 http.get('/status', () => ({ ok: true }));
 
-// FIRST http.post('/v1/user/create', (request, reply) => {});
+const CreateUserSchema = z.object({
+  displayName: z.string().max(255).optional(),
+  name: z.string().max(100),
+  email: z.string().max(255).email(),
+  password: z.string(),
+});
+
+http.post('/v1/user/create', async (request, reply) => {
+  const userData = CreateUserSchema.parse(request.body);
+
+  // user already exists?
+
+  const [newUser] = await db
+    .insert(user)
+    .values(userData)
+    .returning();
+
+  return reply.status(201).send(newUser);
+});
 // http.post('/v1/user/auth', (request, reply) => {});
 
 // If user is authenticated, then they can see other users :)
