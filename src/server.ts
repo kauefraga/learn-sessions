960f2e95a4be06d27517cd3323055578615ff86c
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-11-26 21:38:47 -0300

    Create /v1/user/logout and /v1/user/auth routes (close #3)

--- src/server.ts
+++ src/server.ts
@@ -61,8 +61,82 @@ http.post('/v1/user/create', async (request, reply) => {
     });
 });
 
-// http.post('/v1/user/auth', (request, reply) => {});
-// http.post('/v1/user/logout', (request, reply) => {});
+const AuthUserSchema = z.object({
+  name: z.string().max(100).optional(),
+  email: z.string().max(255).email().optional(),
+  password: z.string(),
+});
+
+http.post('/v1/user/auth', async (request, reply) => {
+  const userSession = await AuthUser(request, db);
+
+  if (userSession) {
+    return reply.status(400).send({
+      message: 'Session already exists.',
+    });
+  }
+
+  const { name, email, password } = AuthUserSchema.parse(request.body);
+
+  if (!(name || email)) {
+    return reply.status(400).send({
+      message: 'At least one field (either name or email) must be exist.',
+    });
+  }
+
+  const emailOrName = email ? eq(user.email, email) : eq(user.name, name ?? '');
+
+  const [existingUser] = await db.select().from(user).where(emailOrName).limit(1);
+
+  if (!existingUser) {
+    return reply.status(400).send({
+      message: 'User does not exist.',
+    });
+  }
+
+  const passwordMatch = await argon2.verify(existingUser.password, password);
+
+  if (!passwordMatch) {
+    return reply.status(400).send({
+      message: 'User name, email or password are invalid.',
+    });
+  }
+
+  const [newSession] = await db
+    .insert(session)
+    .values({ userId: existingUser.id })
+    .returning();
+
+  return reply
+    .cookie('sessionId', newSession.id, {
+      signed: true
+    })
+    .status(201)
+    .send(newSession);
+});
+
+http.delete('/v1/user/logout', async (request, reply) => {
+  const userSession = await AuthUser(request, db);
+
+  if (!userSession) {
+    return reply.status(401).send({
+      message: 'No session to log out.',
+    });
+  }
+
+  const { rowCount } = await db.delete(session).where(eq(session.id, userSession.id));
+
+  if (rowCount === 0) {
+    return reply.status(500).send({
+      message: 'Failed to delete session.'
+    });
+  }
+
+  return reply
+    .clearCookie('sessionId')
+    .status(204)
+    .send();
+});
 
 // TODO PAGINATION
 http.get('/v1/users', async (request, reply) => {
